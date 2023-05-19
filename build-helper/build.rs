81993929ecf71df42d6bbd2c6e85cf7eee72e41b
João Oliveira <hello@jxs.pt>
2023-05-19 12:30:17 +0100

    build.rs copy wasm files and generate abi files
    in artifacts dir

--- build-helper/build.rs
+++ build-helper/build.rs
@@ -1,3 +1,4 @@
+use convert_case::{Case, Casing};
 // Copyright 2023 Polyphene.
 // SPDX-License-Identifier: Apache-2.0, MIT
 use kythera_lib::{self, Abi, Method};
@@ -5,21 +6,24 @@ use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::ops::Deref;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 use std::{fs, thread};
 
 use proc_macro2::TokenTree;
 use syn::{Expr, Item};
 
-const ACTORS: &[&str] = &["hello-world"];
-
 const FILES_TO_WATCH: &[&str] = &["Cargo.toml", "src", "../actors", "../tests"];
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // Cargo executable location.
-    let cargo = std::env::var_os("CARGO").expect("no CARGO env var");
+/// The Kind of actor.
+#[derive(Debug)]
+enum Kind {
+    Target,
+    Test,
+}
 
+/// Generate wasm actors for the input dir.
+fn generate_actors(kind: Kind) -> Result<(), Box<dyn Error>> {
     let out_dir = std::env::var_os("OUT_DIR")
         .as_ref()
         .map(Path::new)
@@ -31,14 +35,42 @@ fn main() -> Result<(), Box<dyn Error>> {
         Path::new(&std::env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR unset"))
             .to_path_buf();
 
-    for file in FILES_TO_WATCH {
-        println!("cargo:rerun-if-changed={}", file);
-    }
+    let workspace_dir = &manifest_dir.parent().expect("Workspace dir should exist");
+
+    let artifacts_dir = workspace_dir.join("artifacts");
+
+    // Cargo executable location.
+    let cargo = std::env::var_os("CARGO").expect("no CARGO env var");
+
+    let path = match kind {
+        Kind::Target => workspace_dir.join("actors"),
+        Kind::Test => workspace_dir.join("tests"),
+    };
+
+    let actor_dirs = fs::read_dir(&path)
+        .expect(&format!("Could not read dir {}", path.display()))
+        .filter_map(|e| e.ok())
+        .map(|e| e.path())
+        .filter(|e| e.is_dir())
+        .filter(|e| {
+            fs::read_dir(e)
+                .unwrap()
+                .filter_map(|e| e.ok())
+                .any(|f| f.path().ends_with("Cargo.toml"))
+        })
+        .collect::<Vec<PathBuf>>();
+
+    let actor_names = actor_dirs
+        .iter()
+        .filter_map(|a| a.file_name())
+        .filter_map(|a| a.to_str())
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
 
     // Cargo build command for all test_actors at once.
     let mut cmd = Command::new(cargo);
     cmd.arg("build")
-        .args(ACTORS.iter().map(|pkg| "-p=".to_owned() + pkg))
+        .args(actor_names.iter().map(|pkg| "-p=".to_owned() + pkg))
         .arg("--target=wasm32-unknown-unknown")
         .arg("--profile=wasm")
         .arg("--locked")
@@ -82,13 +114,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Err("actor build failed".into());
     }
 
-    // Create the Abi file.
-    for actor in ACTORS {
-        let actor_path = manifest_dir
-            .parent()
-            .expect("Workspace dir should exist")
-            .join(format!("actors/{actor}/src/actor.rs"));
-        dbg!(&actor_path);
+    // Create the Abi files and copy the wasm files to the artifacts dir.
+    let actor_source_files = actor_dirs.iter().map(|a| (a, a.join("src/actor.rs")));
+    for (actor_dir, actor_path) in actor_source_files {
         let src = fs::read_to_string(&actor_path)
             .expect(&format!("Could not open {}", actor_path.display()));
         let syntax =
@@ -168,8 +196,38 @@ fn main() -> Result<(), Box<dyn Error>> {
             set_up,
             methods,
         };
-        let actor_name = actor.replace("-", "_");
-        let mut abi_file = File::create(out_dir.join(format!("{actor_name}.cbor"))).unwrap();
+
+        // Create artifacts dir.
+        if let Err(err) = fs::create_dir(&artifacts_dir) {
+            match err.kind() {
+                std::io::ErrorKind::AlreadyExists => {}
+                err => panic!("Could not create artifacts dir {err}"),
+            }
+        };
+
+        // Safe to `unwrap()` as at this time we know the file exists.
+        let actor_name = actor_dir
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            // Rust replaces the -'s in the crate name with _'s,
+            // so we replace it back.
+            .replace("-", "_");
+
+        let actor_wasm_file =
+            out_dir.join(format!("wasm32-unknown-unknown/wasm/{actor_name}.wasm"));
+
+        let actor_dest_name = actor_name.replace("test", ".t").to_case(Case::Pascal);
+
+        fs::copy(
+            &actor_wasm_file,
+            artifacts_dir.join(format!("{actor_dest_name}.wasm")),
+        )
+        .expect("Could not copy wasm file to artifacts dir");
+
+        let mut abi_file =
+            File::create(artifacts_dir.join(format!("{actor_dest_name}.cbor"))).unwrap();
         abi_file
             .write_all(&kythera_lib::to_vec(&abi).unwrap())
             .expect(&format!(
@@ -177,6 +235,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                 actor_path.display()
             ));
     }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    for file in FILES_TO_WATCH {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+
+    generate_actors(Kind::Target).expect("Could not generate target actors");
+    generate_actors(Kind::Test).expect("Could not generate test actors");
 
     Ok(())
 }
