3b5d6120c60223bac358800588b60ac3b04f2c5f
Thomas Chataigner <tom.chataigner@yahoo.fr>
2023-06-20 19:57:04 +0200

    fix: fixed templates to directly use important methods & types

--- templates/test/src/actor.rs
+++ templates/test/src/actor.rs
@@ -1,6 +1,18 @@
-use frc42_dispatch::match_method;
+use cid::{multihash::Code};
+use std::collections::HashMap;
+use frc42_dispatch::{method_hash, match_method};
+use fvm_ipld_blockstore::Block;
+use fvm_ipld_encoding::{RawBytes, DAG_CBOR};
+use fvm_ipld_encoding::ipld_block::IpldBlock;
 use fvm_sdk::NO_DATA_BLOCK_ID;
+use fvm_shared::bigint::Zero;
+use fvm_shared::econ::TokenAmount;
 use fvm_shared::error::ExitCode;
+use fvm_shared::address::Address;
+use fvm_shared::sys::SendFlags;
+use serde_tuple::{Serialize_tuple};
+
+use crate::utils;
 
 #[no_mangle]
 fn invoke(_input: u32) -> u32 {
